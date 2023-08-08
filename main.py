1863e6c2bb7b53c6ee8b3a4faf18d2dabc77e582
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2023-08-08 11:08:29 -0700

    Changed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -6,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 def scrape():
     # Set the URL you want to scrape from
-    url = 'https://codestitch.app/app/dashboard/stitches/1058'
+    url = 'https://codestitch.app/app/dashboard/stitches/758?nav=Top%20Dropdown'
 
     # Set your login credentials
     username = 'lucasmartiniscool@gmail.com'
@@ -18,7 +20,7 @@ def scrape():
     # Connect to the website and fetch the page
     driver.get(url)
 
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(.5)
     # Find the username and password fields
     username_field = driver.find_element(By.ID, 'email')
     password_field = driver.find_element(By.ID, 'pass')
@@ -30,30 +32,30 @@ def scrape():
     # Submit the login form
     password_field.submit()
 
-    # Wait for the page to load
-    driver.implicitly_wait(2)
-
     driver.get(url)
 
     # Get the page source
     html = driver.page_source
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
-
     # Find the first two elements with the 'tab' class
-    tabs = driver.find_elements(By.CLASS_NAME, 'tab')[:2]
+    tabs = driver.find_elements(By.XPATH, '//*[@id="CODE_TABS"]/div[2]/div[contains(@class, "tab")][position() < 4]')[:3]
 
-    # Add the 'active-tab' class to them
-    for tab in tabs:
-        driver.execute_script("arguments[0].classList.add('active-tab')", tab)
+    # Find the textarea elements within the first two tabs
+    text_areas = [tab.find_element(By.TAG_NAME, 'textarea') for tab in tabs]
 
-    # Get the content from the 'CodeMirror-code' class
-    code_elements = driver.find_element(By.CLASS_NAME, 'CodeMirror-code')
-    data = code_elements.text
+    # Get the text data from the two text areas
+    data1 = text_areas[0].get_attribute('value')
+    data2 = text_areas[1].get_attribute('value')
+    data3 = text_areas[2].get_attribute('value')
 
-    # Do something with the data
-    print(data)
+    # Get the filename from the h2 element
+    filename = driver.find_element(By.XPATH, '//*[@id="main"]/div/section/div[1]/div/h2')
+    filename = filename.text
+
+    # Open a new file in write mode
+    with open(filename + '.txt', 'w') as f:
+        # Write the data to the file
+        f.write(data1 + '\n' + data2 + '\n' + data3)
 
     # Close the driver
     driver.close()
