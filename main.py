c63d6259e60b38ea8d4a6e3398308bdd3a2246a7
LucasAMartin <lucasmartiniscool@gmail.com>
2023-08-09 17:53:28 -0700

    Added all urls

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 
 
 def scrape(url, directory):
-
     # Set your login credentials
     username = 'lucasmartiniscool@gmail.com'
     password = 'Seahawksarelit5'
@@ -53,15 +52,13 @@ def scrape(url, directory):
 
             # Find the first two elements with the 'tab' class
             tabs = driver.find_elements(By.XPATH,
-                                        '//*[@id="CODE_TABS"]/div[2]/div[contains(@class, "tab")][position() < 4]')[:3]
+                                        '//*[@id="CODE_TABS"]/div[2]/div[contains(@class, "tab")]')
 
             # Find the textarea elements within the first two tabs
             text_areas = [tab.find_element(By.TAG_NAME, 'textarea') for tab in tabs]
 
-            # Get the text data from the two text areas
-            data1 = text_areas[0].get_attribute('value')
-            data2 = text_areas[1].get_attribute('value')
-            data3 = text_areas[2].get_attribute('value')
+            # Get the text data from all text areas
+            data = [text_area.get_attribute('value') for text_area in text_areas]
 
             # Check if a file with the same name already exists in the buttons directory
             i = 1
@@ -73,7 +70,7 @@ def scrape(url, directory):
             # Open a new file in write mode
             with open(os.path.join(directory, new_filename + '.txt'), 'w', encoding='utf-8') as f:
                 # Write the data to the file
-                f.write(data1 + '\n' + data2 + '\n' + data3)
+                f.write('\n'.join(data))
 
         except Exception as e:
             print(f'An error occurred while saving the file: {e}')
@@ -90,6 +87,58 @@ def scrape(url, directory):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    scrape('https://codestitch.app/app/dashboard/catalog/sections/4', 'Side By Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/1', 'Buttons')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/2', 'Hero')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/2?page=2', 'Hero')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/3', 'Services')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/3?page=2', 'Services')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/3?page=3', 'Services')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/3?page=4', 'Services')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=2', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=3', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=4', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=5', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=6', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/4?page=7', 'Side by Side')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/78', 'Content Flair')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/43', 'Meet Our Team')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/43?page=2', 'Meet Our Team')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/52', 'Steps')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/13', 'Stats')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/49', 'Pricing')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/50', 'FAQ')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/19', 'Why Choose Us')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/62', 'Quotes')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/56', 'Misc')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/20', 'Reviews')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/20?page=2', 'Reviews')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/94', 'Events')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/44', 'Forms and Contact')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/79', 'Blog')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/7', 'Call to Action')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/21', 'Footer')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/55', 'Interior Pages')
+    scrape('https://codestitch.app/app/dashboard/catalog/sections/23', 'Dark Mode')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
